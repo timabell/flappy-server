4a8cb7166facb771500c1a463dab90190b3e38cd
Tim Abell <tim@timwise.co.uk>
2018-11-14 14:49:34 +0000

    200 response

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 app.get('*', (req, res) => {
 	  console.log(new Date(), 'called GET API', req.originalUrl);
 	  setTimeout(() => {
-			    console.log('replying');
-			    res.status(500).send('Something broke!');
-			  }, 2000)
+			    console.log('replying with success');
+			    res.status(200).send('hello from a flappy app service');
+			    // console.log('replying with failure');
+			    // res.status(500).send('Something broke!');
+			  }, 500) // wait 0.5secs
 })
 
 app.listen(3000);
