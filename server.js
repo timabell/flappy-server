d20c124326afac6495aed45b1c158d3f665121e0
Tim Abell <tim@timwise.co.uk>
2018-11-14 15:04:17 +0000

    Return code from env var

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express();
 
+var returnCode = process.env.returncode || 200;
+
 app.get('*', (req, res) => {
 	  console.log(new Date(), 'called GET API', req.originalUrl);
 	  setTimeout(() => {
-			    console.log('replying with success');
-			    res.status(200).send('hello from a flappy app service');
+			    console.log('replying with '+returnCode);
+			    res.status(returnCode).send('hello and a '+returnCode+' from a flappy app service');
 			    // console.log('replying with failure');
 			    // res.status(500).send('Something broke!');
 			  }, 500) // wait 0.5secs
